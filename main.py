5f52ef68ad706c80b0ccf82c5aa9046e7dc83b24
Valletix <Valltix23@gmail.com>
2025-04-29 09:10:12 +0200

    random points now spawn - wohoo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import os
 from enum import Enum
+import random
 
 
 os.environ["SDL_VIDEO_WINDOW_POS"] = "660,240" # Centers the Window for 1080p Screens
@@ -23,9 +24,21 @@ class PlayerDirection(Enum):
     UP = (0, -30)
     DOWN = (0, 30)
 
+class ScorePoint:
+    def __init__(self):
+        self.point_pos = pygame.Vector2(random.randrange(0, 600, 30), random.randrange(0, 600, 30))
+        
+    
+    def draw_point(self):
+        point_rect = pygame.Rect(self.point_pos.x, self.point_pos.y, 30, 30)
+        pygame.draw.rect(screen, "green", rect=point_rect)
+
+    
+
 active_direction = PlayerDirection.RIGHT
 movement_timer = 0
 movement_cooldown = 8
+active_point = None
 
 while running:
     
@@ -33,9 +46,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+
     screen.fill("orange")
+
+    if active_point == None:
+        active_point = ScorePoint()
+    
+    active_point.draw_point()
+
     player_rect = pygame.Rect(player_pos.x, player_pos.y, 30, 30)
     pygame.draw.rect(screen, "purple", rect=player_rect)
+
+   
 
     
 
@@ -54,6 +76,9 @@ while running:
     elif active_direction == PlayerDirection.RIGHT and movement_timer == 8:
         if player_pos.x < 570:
             player_pos.x += active_direction.value[0]
+    
+    if player_pos == active_point.point_pos:
+        active_point = None
     
     if movement_timer == 8:
         movement_timer = 0
@@ -82,8 +107,6 @@ while running:
     
 
     pygame.display.flip()
-
-
 
     clock.tick(60)
     
